c527d2db66863a6278ca4025eee2257ac6a379d6
Mike Murphy <codermurph@sleepymurph.com>
2016-10-19 15:02:51 +0200

    Adjust format of current-runs, switch to Python

--- copy-results.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in 01 02 03 04
-do
-    host="murphytest${i}"
-    filename=$(sed "s/{host}/${host}/; ${i}q;d" current-runs.txt)
-    echo "${host} --> ${filename}"
-    scp ${host}.sleepymurph.com:masters-thesis/code/vc-benchmarks/output.txt \
-        ${filename}
-done
