72aa7bdbac5832597d355adf61dc2fce01d2efa0
Mike Murphy <codermurph@sleepymurph.com>
2017-03-28 16:52:17 +0200

    Start new file-size test runs

--- test-results/copy-results.py
+++ test-results/copy-results.py
@@ -2,7 +2,8 @@
 
 import subprocess
 
-remotefname = 'masters-thesis/code/vc-benchmarks/output.txt'
+remotefname = 'masters-thesis/test-code/vc-benchmarks/output.txt'
+oldremotefname = 'masters-thesis/code/vc-benchmarks/output.txt'
 domain = 'sleepymurph.com'
 
 with open('current-runs.txt') as f:
@@ -12,4 +13,9 @@ with open('current-runs.txt') as f:
         dst = dst.replace('{host}', host)
         src = "{0}.{1}:{2}".format(host, domain, remotefname)
         print src, ' --> ', dst
-        subprocess.call(['scp', src, dst])
+        exitcode = subprocess.call(['scp', src, dst])
+        if exitcode != 0:
+            print "output not found in new location, checking old"
+            src = "{0}.{1}:{2}".format(host, domain, oldremotefname)
+            print src, ' --> ', dst
+            exitcode = subprocess.call(['scp', src, dst])
